08033e975b3f753c137b6b7c62430b3cfd6ea0de
Larissa Sales <larissa.s.sales@outlook.com>
2017-12-06 10:28:01 -0200

    Add devise gem
    Signed-off-by: Larissa Sales <larissa.s.sales@outlook.com>

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,7 +12,9 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  #add
   Paperclip.options[:command_path] = "/usr/bin/convert"
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Enable/disable caching. By default caching is disabled.
   if Rails.root.join('tmp/caching-dev.txt').exist?
