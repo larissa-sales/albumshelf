32f26f4b2669359a046e899809acd7d1e3aba5b3
Larissa Sales <larissa.s.sales@outlook.com>
2017-12-07 08:10:49 -0200

    Add fields in sign up form
    Signed-off-by: Larissa Sales <larissa.s.sales@outlook.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171206160021) do
+ActiveRecord::Schema.define(version: 20171207100812) do
 
   create_table "albums", force: :cascade do |t|
     t.string "title"
@@ -39,6 +39,9 @@ ActiveRecord::Schema.define(version: 20171206160021) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "user_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
