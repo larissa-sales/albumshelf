d784d0db6ec3d7d9c5b78d8e751a56dd1e47af3b
Larissa Sales <larissa.s.sales@outlook.com>
2017-12-09 19:04:37 -0200

    Add avatar in navbar
    Signed-off-by: Larissa Sales <larissa.s.sales@outlook.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_attached_file :avatar, :styles => { :large => "1000x1000", :medium => "600x600>", :thumb => "150x150>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
   has_many :albums
   has_many :books
   validates_uniqueness_of :user_name
