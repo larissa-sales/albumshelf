3b6cd62643e43d26d2de9bcc61b9147d2229bb21
Larissa Sales <larissa.s.sales@outlook.com>
2017-12-10 01:14:31 -0200

    Changed default images
    Signed-off-by: Larissa Sales <larissa.s.sales@outlook.com>

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,5 +1,5 @@
 class Album < ApplicationRecord
-  has_attached_file :cover, :styles => { :large => "1000x1000", :medium => "600x600>", :thumb => "150x150>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :cover, :styles => { :large => "1000x1000", :medium => "600x600>", :thumb => "150x150>" }, :default_url => "/images/:style/none.png"
   validates_attachment_content_type :cover, :content_type => /\Aimage\/.*\Z/
   belongs_to :user
   has_and_belongs_to_many :books
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_attached_file :avatar, :styles => { :large => "1000x1000", :medium => "600x600>", :thumb => "150x150>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, :styles => { :large => "1000x1000", :medium => "600x600>", :thumb => "150x150>" }, :default_url => ""
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
   has_many :albums
