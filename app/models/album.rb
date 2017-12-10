908453357fc12f77cef1cb1432e7c296a8ebca63
Alexandre Miguel <aleronupe@hotmail.com>
2017-12-10 14:47:54 -0200

    Adding Hello and Changing Image Display Sizes
    Signed-off-by: Alexandre Miguel <aleronupe@hotmail.com>

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,5 +1,5 @@
 class Album < ApplicationRecord
-  has_attached_file :cover, :styles => { :large => "600x600", :medium => "300x300>", :thumb => "150x150>" }, :default_url => "/images/:style/none.png"
+  has_attached_file :cover, :styles => { :large => "450x450", :medium => "300x300>", :thumb => "150x150>" }, :default_url => "/images/:style/none.png"
   validates_attachment_content_type :cover, :content_type => /\Aimage\/.*\Z/
   belongs_to :user
   has_and_belongs_to_many :books
